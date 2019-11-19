0d9d26031585650cc98646d7b640123cf2a89f4f
Davi Lino <mortuum26@gmail.com>
2019-11-19 14:44:53 -0300

    Added Home

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import api from '../../services/api';
 
 export default function Home({ history }){
     const user = localStorage.getItem('user');
     const name = localStorage.getItem('name');
+    const [subjects, setSubjects] = useState([]);
 
     function logout(event){
         event.preventDefault();
@@ -12,10 +13,27 @@ export default function Home({ history }){
         history.push('/');
     }
 
+    useEffect(() => {
+        async function loadSubjects() {
+          const response = await api.get('/subjects');
+    
+          console.log(response);
+          setSubjects(response.data);
+        }
+    
+        loadSubjects();
+      }, []);
+
     return (
         <>  Bem Vindo {`${name}`}
-
-        
+            <ul className="subjects">
+                {subjects.map(subject => (
+                <li key={subject._id}>
+                    <span><strong>{subject.name}</strong> - {subject.cod}</span>
+                </li>
+                ))}
+            </ul>
+            
             <br/>
             <button className='btn' onClick={logout}>Logout</button>
         </>
